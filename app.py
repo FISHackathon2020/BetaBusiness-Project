2672dac5741390031d498b153e4a541afb0d6cb0
Carson Hillebrand <carsonhillebrand@Carsons-MacBook-Pro-2.local>
2020-08-29 20:01:11 -0400

    Finished front end

--- app.py
+++ app.py
@@ -1,11 +1,17 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, flash, url_for
+from forms import MainForm
 
 app = Flask(__name__)
+app.config['SECRET_KEY'] = '516d72896e5d87cca4df6acb7caaedcc'
 
 
-@app.route('/')
+@app.route('/', methods=['GET', 'POST'])
 def index():
-    return render_template('index.html')
+    form = MainForm()
+    if form.validate_on_submit():
+        flash('Thank you for your submission, we will be emailing you shortly!', 'success')
+
+    return render_template('index.html', form=form)
 
 
 if __name__ == '__main__':
--- forms.py
+++ forms.py
@@ -1,8 +1,15 @@
 from flask_wtf import FlaskForm
-from wtforms import StringField
-from wtforms.validators import DataRequired, Length
+from flask_wtf.file import FileField, FileAllowed
+from wtforms import StringField, SubmitField
+from wtforms.validators import DataRequired, Length, Email
 
 
 class MainForm(FlaskForm):
-    name = StringField('Name', validators=[
-                       DataRequired(), Length(min=2, max=40)])
+    fname = StringField('First Name', validators=[
+        DataRequired(), Length(min=1, max=20)])
+    lname = StringField('Last Name', validators=[
+        DataRequired(), Length(min=1, max=30)])
+    email = StringField('Email', validators=[DataRequired(), Email()])
+    resume = FileField('Submit Resume (pdf only)', validators=[
+        FileAllowed(['pdf'])])
+    submit = SubmitField('Submit')

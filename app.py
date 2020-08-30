51f1b9f73f3ddb5ba05689b43e3333b0396e0dbd
Carson Hillebrand <carsonhillebrand@Carsons-MacBook-Pro-2.local>
2020-08-30 05:41:22 -0400

    Finished project

--- app.py
+++ app.py
@@ -3,13 +3,14 @@ from flask import Flask, render_template, flash, url_for, request
 from flask_sqlalchemy import SQLAlchemy
 from forms import MainForm
 from werkzeug.utils import secure_filename
+from scraper import applicant, extract_text_from_pdf
 
 app = Flask(__name__)
-app.config['SECRET_KEY'] = '516d72896e5d87cca4df6acb7caaedcc'
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///site.db'
-app.config['MAX_CONTENT_LENGTH'] = 1024 * 1024
-app.config['UPLOAD_EXTENSIONS'] = ['.pdf']
-app.config['UPLOAD_PATH'] = 'static/resumes'
+app.config["SECRET_KEY"] = "516d72896e5d87cca4df6acb7caaedcc"
+app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///site.db"
+app.config["MAX_CONTENT_LENGTH"] = 1024 * 1024
+app.config["UPLOAD_EXTENSIONS"] = [".pdf"]
+app.config["UPLOAD_PATH"] = "static/resumes"
 
 
 db = SQLAlchemy(app)
@@ -26,26 +27,47 @@ class Applicant(db.Model):
     score3 = db.Column(db.Integer, nullable=True)
 
     def __repr__(self):
-        return f'{self.first_name} {self.last_name}, {self.email}, {self.resume}, Score 1: {self.score1}, Score 2: {self.score2}, Score 3: {self.score3}'
+        return f"{self.first_name} {self.last_name}, {self.email}, {self.resume}, Score 1: {self.score1}, Score 2: {self.score2}, Score 3: {self.score3}"
 
 
-@app.route('/', methods=['GET', 'POST'])
+def get_scores(filename):
+    file = open(filename, "rb")
+    temp = extract_text_from_pdf(file)
+    app = applicant(temp)
+    return app.getWinAsOneTeam(), app.getLeadWithIntegrity(), app.getBeTheChange()
+    # print(app.GPA)
+
+
+@app.route("/", methods=["GET", "POST"])
 def index():
     form = MainForm()
     if form.validate_on_submit():
         f = form.resume.data
         filename = secure_filename(f.filename)
-        applicant = Applicant(first_name=form.fname.data, last_name=form.lname.data,
-                              email=form.email.data, resume=filename)
-        f.save(os.path.join(app.config['UPLOAD_PATH'], filename))
+        f.save(os.path.join(app.config["UPLOAD_PATH"], filename))
+        score1, score2, score3 = get_scores(
+            os.path.join(app.config["UPLOAD_PATH"], filename)
+        )
+        applicant = Applicant(
+            first_name=form.fname.data,
+            last_name=form.lname.data,
+            email=form.email.data,
+            resume=filename,
+            score1=score1,
+            score2=score2,
+            score3=score3,
+        )
+
         db.session.add(applicant)
         db.session.commit()
-        flash('Thank you for your submission, we will be emailing you shortly!', 'success')
+        flash(
+            "Thank you for your submission, we will be emailing you shortly!", "success"
+        )
     else:
-        flash('There was an error with your submission', 'danger')
-    return render_template('index.html', form=form)
+        flash("There was an error with your submission", "danger")
+    return render_template("index.html", form=form)
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     app.debug = True
     app.run()
--- scraper.py
+++ scraper.py
@@ -6,11 +6,12 @@ from pdfminer.pdfinterp import PDFPageInterpreter
 from pdfminer.pdfinterp import PDFResourceManager
 from pdfminer.pdfpage import PDFPage
 
+
 class applicant:
     GPA = 0
     WinAsOneTeam = 0
     LeadWithIntegrity = 0
-    BeTheChange =0
+    BeTheChange = 0
 
     def __init__(self, file):
         splits = str.split(file)
@@ -18,74 +19,73 @@ class applicant:
         btc = pd.read_csv("BeTheChange.csv")
         lwi = pd.read_csv("LeadWithIntegrity.csv")
         waot = pd.read_csv("WinAsOneTeam.csv")
-  
+
         for split in splits:
 
             for item in btc:
                 if item.lower() == split.lower():
-                    applicant.BeTheChange=applicant.BeTheChange+1
+                    applicant.BeTheChange = applicant.BeTheChange + 1
 
             for item2 in lwi:
                 if item2.lower() == split.lower():
-                    applicant.LeadWithIntegrity=applicant.LeadWithIntegrity+1
+                    applicant.LeadWithIntegrity = applicant.LeadWithIntegrity + 1
 
             for item3 in waot:
                 if item3.lower() == split.lower():
-                    applicant.WinAsOneTeam=applicant.WinAsOneTeam+1
+                    applicant.WinAsOneTeam = applicant.WinAsOneTeam + 1
 
             if "." in split:
-                if split.replace('.', '', 1).isdigit():
+                if split.replace(".", "", 1).isdigit():
                     applicant.GPA = float(split)
 
     def getWinAsOneTeam(self):
-        if(applicant.WinAsOneTeam>3):
+        if applicant.WinAsOneTeam > 7:
             return 3
-        elif(applicant.WinAsOneTeam>1):
+        elif applicant.WinAsOneTeam > 1:
             return 2
         else:
             return 1
 
     def getLeadWithIntegrity(self):
-        if(applicant.LeadWithIntegrity>3):
+        if applicant.LeadWithIntegrity > 7:
             return 3
-        elif(applicant.LeadWithIntegrity>1):
+        elif applicant.LeadWithIntegrity > 1:
             return 2
         else:
             return 1
 
     def getBeTheChange(self):
-        if(applicant.BeTheChange>3):
+        if applicant.BeTheChange > 7:
             return 3
-        elif(applicant.BeTheChange>1):
+        elif applicant.BeTheChange > 1:
             return 2
         else:
             return 1
+
 
 def extract_text_from_pdf(fh):
     resource_manager = PDFResourceManager()
     fake_file_handle = io.StringIO()
     converter = TextConverter(resource_manager, fake_file_handle)
     page_interpreter = PDFPageInterpreter(resource_manager, converter)
-    
-    for page in PDFPage.get_pages(fh, 
-                                caching=True,
-                                check_extractable=True):
+
+    for page in PDFPage.get_pages(fh, caching=True, check_extractable=True):
         page_interpreter.process_page(page)
-            
+
         text = fake_file_handle.getvalue()
 
     converter.close()
     fake_file_handle.close()
-    
+
     if text:
         return text
-    
 
-##===========================================Driver
-file = open('Saha Cazenove Resume.pdf', 'rb') 
+
+# ===========================================Driver
+""" file = open("Saha Cazenove Resume.pdf", "rb")
 temp = extract_text_from_pdf(file)
 app = applicant(temp)
 print(app.getWinAsOneTeam())
 print(app.getLeadWithIntegrity())
 print(app.getBeTheChange())
-print(app.GPA)
+print(app.GPA) """
